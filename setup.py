99deca37934bf7e4e7589d43089b4072b3f2b1c4
kushalkolar <kushalkolar@gmail.com>
2019-12-28 11:48:02 +0100

    include all subpackages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='fcsugar',
     version='0.1.0',
-    packages=['fcsugar'],
+    packages=find_packages(),
     url='https://github.com/kushalkolar/composition_sugar',
     license='GPL v3',
     author='Kushal Kolar',
